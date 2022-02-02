fae6787c8895dda34966108b9cc6297f5c9d8218
Tejas Amle <tejas.amle@moveinsync.com>
2022-02-02 23:59:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/manosupermarket/view/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.manosupermarket.view
+
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.example.manosupermarket.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/manosupermarket/viewmodel/MainAcitivityViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.manosupermarket.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.manosupermarket.model.DataModel
+
+class MainAcitivityViewModel: ViewModel() {
+
+    var orderModel: DataModel? =null
+
+
+}
